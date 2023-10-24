d697d69da1432e3893ae78173ba01b113045c87f
Wasif0787 <wasifhussain787@gmail.com>
2023-10-24 15:49:07 +0530

    first commit

--- app.js
+++ app.js
@@ -1,59 +1,128 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const lodash = require("lodash")
-let posts = [];
+const lodash = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Welcome to our blog, a virtual canvas where curiosity takes the lead. Immerse yourself in a realm of captivating narratives, insightful explorations, and thought-provoking discussions. With each post, we embark on a quest to unravel mysteries, celebrate creativity, and deepen our understanding of the world around us.From the mesmerizing dance of distant galaxies to the rich tapestry of human history, our blog is a gateway to endless inspiration. Join us as we navigate the realms of science, art, culture, and beyond. As you embark on this journey, let your imagination roam free and your intellect flourish.Here, we invite you to not only be a reader but an active participant. Engage in conversations, share your thoughts, and let your curiosity guide you. Together, we'll embark on an exhilarating expedition of knowledge and discovery. Welcome to our blog, where the extraordinary awaits your exploration.";
+const port = process.env.PORT || 3000;
 
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+// let posts = [];
+
+const homeStartingContent =
+  "Welcome to our blog, a virtual canvas where curiosity takes the lead. Immerse yourself in a realm of captivating narratives, insightful explorations, and thought-provoking discussions. With each post, we embark on a quest to unravel mysteries, celebrate creativity, and deepen our understanding of the world around us.From the mesmerizing dance of distant galaxies to the rich tapestry of human history, our blog is a gateway to endless inspiration. Join us as we navigate the realms of science, art, culture, and beyond. As you embark on this journey, let your imagination roam free and your intellect flourish.Here, we invite you to not only be a reader but an active participant. Engage in conversations, share your thoughts, and let your curiosity guide you. Together, we'll embark on an exhilarating expedition of knowledge and discovery. Welcome to our blog, where the extraordinary awaits your exploration.";
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const password = encodeURIComponent("#Wasif#blogv2786#");
+mongoose
+  .connect(
+    `mongodb+srv://wasifhussain787:${password}@blogv2.dae83ee.mongodb.net/blogDB`
+  )
+  .then(() => {
+    console.log("Connected successfylly");
+  });
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  author: String, // Add a field for the author's name
+  comments: [
+    {
+      text: String,
+      author: String, // Add a field for the comment author's name
+      timestamp: Date,
+    },
+  ],
+});
+
+const Post = mongoose.model("Post", postSchema);
+
 app.get("/", function (req, res) {
-  res.render("home.ejs", { startingContent: homeStartingContent, postsArray: posts });
-})
+  Post.find().then((foundPost) => {
+    res.render("home.ejs", {
+      startingContent: homeStartingContent,
+      postsArray: foundPost,
+    });
+  });
+});
 
 app.get("/about", function (req, res) {
   res.render("about.ejs");
-})
+});
 
 app.get("/contact", function (req, res) {
   res.render("contact.ejs");
-})
+});
 
 app.get("/compose", function (req, res) {
   res.render("compose.ejs");
-})
+});
 
-app.get("/posts/:postName", function (req, res) {
-
-  var requestedTitle = lodash.lowerCase(req.params.postName);
-
-  posts.forEach(function (post) {
-    const storedTitle = lodash.lowerCase(post.title);
-    if (storedTitle === requestedTitle) {
-      console.log("Mathc Found");
-      res.render("post.ejs", { postTitle: post.title, postBody: post.content })
-    }
-  })
-})
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
+  Post.findOne({ _id: requestedPostId }).then((foundPost) => {
+    res.render("post", {
+      postID: requestedPostId,
+      postTitle: foundPost.title,
+      postBody: foundPost.content,
+      postUser: foundPost.author, // Change to foundPost.author
+      comments: foundPost.comments,
+    });
+  });
+});
 
 app.post("/compose", function (req, res) {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
-    content: req.body.postBody
-  }
-  posts.push(post);
-  res.redirect("/");
-})
+    content: req.body.postBody,
+    author: req.body.postBy,
+  });
+  post.save().then(() => {
+    res.redirect("/");
+  });
+  // const post = {
+  //   title: req.body.postTitle,
+  //   content: req.body.postBody
+  // }
+  // posts.push(post);
+  // res.redirect("/");
+});
 
+app.post("/posts/:postId/comments", function (req, res) {
+  const postId = req.params.postId;
+  const commentAuthor = req.body.commentAuthor; // Get the comment author's name from the form
+  const commentText = req.body.comment;
 
-app.listen(3000, function () {
+  // Find the post by ID and update its comments using Promises
+  Post.findOne({ _id: postId })
+    .then((foundPost) => {
+      if (!foundPost) {
+        return res.status(404).send("Post not found");
+      }
+
+      const newComment = {
+        text: commentText,
+        author: commentAuthor, // Add the author's name to the comment
+        timestamp: new Date(),
+      };
+
+      foundPost.comments.push(newComment);
+      return foundPost.save();
+    })
+    .then(() => {
+      res.redirect("/posts/" + postId);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("An error occurred");
+    });
+});
+
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
